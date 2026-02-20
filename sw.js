7597b9540387923b196074b6d4a1941d41635c52
JESSE <jessethegreat6190@gmail.com>
2026-02-20 10:25:50 +0300

    Commit 8
    Fixted youtube link

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'grace-church-v1';
+const CACHE_NAME = 'grace-church-v3';
 const ASSETS = [
   '/',
   '/index.html',
@@ -10,6 +10,16 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
     })
   );
 });
